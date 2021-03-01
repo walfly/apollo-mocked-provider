d9e8b34ef3224b66942700d5c94367d6d53fa0c9
walker flynn <walker.d.flynn@gmail.com>
2021-03-01 01:08:35 -0800

    Update index.ts
    remove fetch

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ export * from './createApolloErrorProvider';
 export * from './createApolloLoadingProvider';
 export * from './createApolloMockedProvider';
 export * from './ApolloMockedProviderOptions';
-export * from './fetchTypeDefs';
